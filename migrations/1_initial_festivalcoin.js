735f6b2974f312b25a7af88b9921cc8f91b28005
BringMaster <icraftbg@gmail.com>
2021-02-10 13:59:53 +0200

    Update 1_initial_festivalcoin.js

--- migrations/1_initial_festivalcoin.js
+++ migrations/1_initial_festivalcoin.js
@@ -1,5 +1,14 @@
-var FESTToken = artifacts.require("FESTToken");
+var FestivalToken = artifacts.require("FestivalToken");
 
 module.exports = function(deployer) {
   deployer.deploy(FESTToken);
+  deployer.deploy(Freezer);
+  deployer.deploy(IBurn);
+  deployer.deploy(IERC20);
+  deployer.deploy(IMint);
+  deployer.deploy(Locker);
+  deployer.deploy(Minter);
+  deployer.deploy(Ownable);
+  deployer.deploy(Pauser);
+  deployer.deploy(SafeMath);
 };
